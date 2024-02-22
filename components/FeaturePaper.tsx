c429e3dc6f249b27aa1d9670da974035ebecbd9c
joseph maldjian <josephavm@icloud.com>
2024-02-22 18:53:02 +0000

    Allow links on FeaturePaper

--- components/FeaturePaper.tsx
+++ components/FeaturePaper.tsx
@@ -1,7 +1,7 @@
 import { Paper, Center, Flex, Title, Text, Stack } from "@mantine/core";
-import { ReactNode } from "react";
+import { ElementType, ReactNode } from "react";
 import classes from "./FeaturePaper.module.css"
-export default function FeaturePaper({ icon, title, children, mb }: React.PropsWithChildren<{ icon: ReactNode, title: string, mb?: string | number }>) {
+export default function FeaturePaper({ icon, title, children, mb, component, href }: React.PropsWithChildren<{ icon: ReactNode, title: string, mb?: string | number, component?: any, href?: any }>) {
     return (
         <Paper
             mb={mb}
@@ -11,16 +11,18 @@ export default function FeaturePaper({ icon, title, children, mb }: React.PropsW
                 backgroundColor: "#18181d",
                 padding: 25,
                 paddingTop: 0,
-                marginTop: 60
+                marginTop: 60,
+                textDecoration: "none",
+                color: "inherit"
             }}
-            className={classes.paper}        >
+            className={classes.paper} component={component} href={href}>
             <Center style={{ height: 0, marginBottom: 50 }}>
                 {icon}
             </Center>
 
             <Flex direction="row" style={{ marginBottom: 10 }}>
                 <Title
-                    style={{ cursor: title.includes("¹") ? "pointer" : "auto", textAlign: "center" }}
+                    style={{ cursor: title.includes("¹") ? "pointer" : "auto", textAlign: "center", color: "inherit" }}
                     order={3}
                     size="h2"
                     mx="auto"
@@ -30,7 +32,7 @@ export default function FeaturePaper({ icon, title, children, mb }: React.PropsW
             </Flex>
 
             <Stack>
-                <Center>
+                <Center style={{color: "inherit"}}>
                     {children}
                 </Center>
             </Stack>
